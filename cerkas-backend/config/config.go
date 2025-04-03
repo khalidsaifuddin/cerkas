059464a6ea2c3d2e2d440642e673f14cc2da8689
Khalid Saifuddin <khalid@ruangguru.com>
2025-04-03 07:49:56 +0700

    env config

--- cerkas-backend/config/config.go
+++ cerkas-backend/config/config.go
@@ -10,11 +10,11 @@ type Config struct {
 	HTTPPort    string `envconfig:"HTTP_PORT" default:"8080"`
 	Environment string `envconfig:"ENVIRONMENT" default:"staging"`
 
-	Host            string `envconfig:"PPDB_BACKEND_PGSQL_HOST" default:"103.41.206.233"`
-	Port            string `envconfig:"PPDB_BACKEND_PGSQL_PORT" default:"5432"`
-	Username        string `envconfig:"PPDB_BACKEND_PGSQL_USERNAME" default:"postgres"`
-	Password        string `envconfig:"PPDB_BACKEND_PGSQL_PASSWORD" default:"r00t_db"`
-	DBName          string `envconfig:"PPDB_BACKEND_PGSQL_DBNAME" default:"simdik"`
+	Host            string `envconfig:"CERKAS_PGSQL_HOST" default:""`
+	Port            string `envconfig:"CERKAS_PGSQL_PORT" default:""`
+	Username        string `envconfig:"CERKAS_PGSQL_USERNAME" default:""`
+	Password        string `envconfig:"CERKAS_PGSQL_PASSWORD" default:""`
+	DBName          string `envconfig:"CERKAS_PGSQL_DBNAME" default:""`
 	LogMode         bool   `envconfig:"DB_LOG_MODE" default:"true"`
 	MaxIdleConns    int    `envconfig:"DB_MAX_IDLE_CONNS" default:"5"`
 	MaxOpenConns    int    `envconfig:"DB_MAX_OPEN_CONNS" default:"10"`
@@ -27,7 +27,7 @@ type Config struct {
 	RedisMaxIdle  int    `envconfig:"REDIS_MAX_IDLE" default:"10"`
 	DefaultTTL    int64  `envconfig:"DEFAULT_TTL" default:"3600"`
 
-	PSPInternalSecretKey string `envconfig:"PSP_INTERNAL_SECRET_KEY" default:"PSP_INTERNAL_SECRET_KEY"`
+	InternalSecretKey string `envconfig:"INTERNAL_SECRET_KEY" default:"INTERNAL_SECRET_KEY"`
 }
 
 func Get() Config {
